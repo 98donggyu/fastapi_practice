6fc340d5cec5e1c0b13fe46e4cbf838636691658
98donggyu <98donggyu@gmail.com>
2025-05-30 15:13:29 +0900

    todo 만들기

--- main.py
+++ main.py
@@ -46,3 +46,27 @@ async def add(request: Request, task: str = Form(...), db: Session = Depends(get
     db.add(todo)
     db.commit()
     return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
+
+@app.get("/edit/{todo_id}")
+async def add(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    todos = db.query(models.Todo).order_by(models.Todo.id.desc())
+    return templates.TemplateResponse(
+        "edit.html", 
+        {"request": request, "todo": todo, "todos" : todos}
+        )
+
+@app.post("/edit/{todo_id}")
+async def add(request: Request, todo_id: int, task: str = Form(...), completed: bool = Form(False), db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    todo.task = task
+    todo.completed = completed
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
+
+@app.get("/delete/{todo_id}")
+async def add(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    db.delete(todo)
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
